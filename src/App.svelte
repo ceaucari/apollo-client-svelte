d08376a689da429caa1244dedd25c8f2641b2c84
Cesar Castillo <ceaucari@gmail.com>
2020-07-10 21:06:14 -0500

    Add routing with https://visionmedia.github.io/page.js/

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,38 @@
 <script>
-	export let name;
+  import router from 'page';
+
+  // Include our Routes
+  import Home from './routes/Home.svelte';
+  import Blog from './routes/Blog.svelte';
+  import SingleBlog from './routes/SingleBlog.svelte';
+
+  // Variables
+  let page;
+  let params;
+
+  // Set up the pages to watch for
+  router('/', () => (page = Home));
+  router('/blog', () => (page = Blog));
+  router(
+    '/blog/:id',
+    // Before we set the component
+    (ctx, next) => {
+      params = ctx.params;
+      next();
+    },
+    // Finally set the component
+    () => (page = SingleBlog)
+  );
+
+  // Set up the router to start and actively watch for changes
+  router.start();
 </script>
 
+<nav>
+  <a href="/">Home</a>
+  <a href="/blog">Blog</a>
+</nav>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <svelte:component this="{page}" params="{params}" />
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
